ab9473f186c6de0a5402b095f3dea7eb32c4ac84
rmatouschekh <rhicke@gmail.com>
2017-01-09 10:30:03 -0600

    added possible constructor. add(value), and clear

--- slist.cpp
+++ slist.cpp
@@ -5,16 +5,87 @@
 Class Library File
 
 */
+#include <iostream>
+#include <string>
+using namespace std;
 
+void LinkedList::Last()
+{
+   if(current != NULL)
+   { 
+      while(current -> pNext != NULL)
+      {
+        current = current -> pNext;
+      }
+   }
+}
 
 // Constructor
+class LinkedList
+{
+  private:
+  struct Node
+  {
+    string name;
+    Node *pNext;
+  };
+  Node *current;
+  Node *first;
+public:
+  LinkedList();
+  LinkedList(string);
+  void Last();
+  void addList(string);
+  void displayList();
+};
 
+LinkedList::LinkedList()
+{
+    current = NULL;
+    first = NULL;
+}
+
+LinkedList::LinkedList(string nm)
+{
+
+    current = first = new Node;
+    first -> name = nm;
+    first -> pNext = NULL;
+}
 // Destructor
 
 // add(value)				//Adds a new value to the end of this list.
-
+void LinkedList::addList(string nm)
+{
+  if(current == NULL)            // empty list
+  {
+    current = new Node;
+    current -> name = nm;
+    current -> pNext = NULL;
+    first = current;
+  }
+  else
+  {
+    Last();
+    current -> pNext = new Node;
+    current = current -> pNext;
+    current -> name = nm;
+    current -> pNext = NULL;
+  }
+}
 // clear()					//Removes all elements from this list.
-
+void LinkedList::clear()
+{
+  Node* ha = first; // empty header
+  Node* current = first->pNext; // first valid node
+  while (current != NULL)
+  {
+    ha->pNext = current->pNext; // unlike node removing
+    delete current; // delete the node
+    ha = current; // set new ha 
+    current = current->pNext; // move current forwards
+  }
+}
 // equals(list)				//Returns true if the two lists contain the same elements in the same order.
 
 //get(index)				//Returns the element at the specified index in this list.
@@ -34,3 +105,6 @@ Class Library File
 // subList(start, length)	//Returns a new list containing elements from a sub-range of this list.
 
 // toString()				//Converts the list to a printable string representation.
+void LinkedList::toString()
+{
+}
